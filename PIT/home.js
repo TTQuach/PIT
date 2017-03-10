10627a9ba3f837fb51611a6d0966826772bb5059
TQUACH <TQUACH@LBNL01446>
2017-03-10 11:09:08 +0100

    industries

--- PIT/home.js
+++ PIT/home.js
@@ -1,1 +1,57 @@
-﻿
+﻿var app = angular.module('myApp', []);
+
+app.controller("industry", function ($scope) {
+
+    $scope.industries = [{
+        definition: 'Aerospace & Defence',
+        text: 'Hmm i like Aerospaceeee',
+        images: ["icons/industries/aerospace.png"]
+    }, {
+        definition: 'Industrial Products',
+        text: 'Love industrial',
+        images: ["icons/industries/industrial.png"]
+    }, {
+        definition: 'Automotive',
+        text: 'BLABLABLABLA'
+    }, {
+        definition: 'Insurance',
+        text: 'BLABLABLABLA'
+    }, {
+        definition: 'Banking & Capital Markets',
+        text: 'BLABLABLABLA'
+    }, {
+        definition: 'Life Sciences',
+        text: 'BLABLABLABLA'
+    }, {
+        definition: 'Consumer Products & Retail',
+        text: 'BLABLABLABLA'
+    }, {
+        definition: 'Manufacturing',
+        text: 'BLABLABLABLA'
+    }, {
+        definition: 'Distribution & Transportation',
+        text: 'BLABLABLABLA'
+    }, {
+        definition: 'Oil & Gas, Chemicals',
+        text: 'BLABLABLABLA'
+    }, {
+        definition: 'Healthcare',
+        text: 'BLABLABLABLA'
+    }, {
+        definition: 'Public Sector',
+        text: 'BLABLABLABLA'
+    }, {
+        definition: 'High Tech',
+        text: 'BLABLABLABLA'
+    }, {
+        definition: 'Telecoms & Entertainment',
+        text: 'BLABLABLABLA'
+    }, {
+        definition: 'Utilities',
+        text: 'BLABLABLABLA'
+    }, {
+        definition: 'Other',
+        text: 'BLABLABLABLA'
+
+    }];
+});
